ca5c4541d12d14ce494c7abab403f888527abfed
Vivouskaya <vivou_kurt@hotmail.fr>
2014-10-20 17:46:50 +0200

    caroussel

---
+++ css/carousselcustom.js
@@ -0,0 +1,17 @@
+
+
+    $(document).ready(function() {
+     
+    $("#owl-demo").owlCarousel({
+    autoPlay : 3000,
+    stopOnHover : true,
+    navigation:true,
+    paginationSpeed : 1000,
+    goToFirstSpeed : 2000,
+    singleItem : true,
+    autoHeight : true,
+    transitionStyle:"fade"
+    });
+     
+    });
+
